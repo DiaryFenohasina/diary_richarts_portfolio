68666d5e81f2de24e4898234a066ceacaaf9a122
richartsdiaryfenohasina <richartsdiaryfenohasina@gmail.com>
2025-11-28 14:36:11 +0300

    feat/feat-header

--- src/components/HeaderComponents.tsx
+++ src/components/HeaderComponents.tsx
@@ -1,13 +1,14 @@
 import Icon from '../assets/Icon.png';
 function HeaderComponents() {
     return (
-        <header className="relative flex items-center p-8 ">
+        <header className="relative flex items-center p-8">
             {/* Menu centré */}
             <div className="mx-auto border border-gray-300 rounded-2xl shadow-md px-6 py-2">
                 <nav>
                     <ul className="flex space-x-10">
-                        <li><a href="#home" className="text-gray-600 font-bold hover:underline">Home</a></li>
+                        <li><a href="#home" className="text-black font-bold hover:text-gray-600">Home</a></li>
                         <li><a href="#about" className="text-gray-600 font-bold hover:underline">About</a></li>
+                        <li><a href="#project" className="text-gray-600 font-bold hover:underline">Project</a></li>
                         <li><a href="#contact" className="text-gray-600 font-bold hover:underline">Contact</a></li>
                     </ul>
                 </nav>
