2137c4d7de966c7bf8620631ab2b6c1d819bbdc3
richartsdiaryfenohasina <richartsdiaryfenohasina@gmail.com>
2026-02-02 11:54:51 +0300

    feat/feat-app

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,9 @@
-// import './App.css'
-import HomeViews from "./views/HomeViews"
-import SectionHorizontal from "./views/About"
-import Glitch from "./views/Glitch"
-import HeaderComponents from "./components/HeaderComponents"
+import AppRoutes from "./Router/Routes"
+import './App.css'
 
 function App() {
   return (
-    <div className="bg-white relative min-h-screen w-screen overflow-hidden">
-      <HeaderComponents />
-      <HomeViews />
-      {/* <SectionHorizontal /> */}
-      {/* <Glitch words={["Glitch", "Effect"]} /> */}
-    </div>
+    <AppRoutes></AppRoutes>
   )
 }
 
