95a14661b37e5e2c71d13e2a4ec965bee55a7a0c
richartsdiaryfenohasina <richartsdiaryfenohasina@gmail.com>
2026-02-16 14:48:13 +0300

    feat

--- src/data/SkillsMock.ts
+++ src/data/SkillsMock.ts
@@ -23,7 +23,7 @@ const skillsMock = [
   {
     tech: "DevOps & Outils",
     icon: "devops",
-    stack: ["Docker", "Git", "Kafka", "Redis"],
+    stack: ["Docker", "Git", "Kafka", "Redis", "Meilisearch"],
     fr: "Docker, Git, Kafka, Redis. Containerisation, gestion de version, communication asynchrone et optimisation des performances.",
     en: "Docker, Git, Kafka, Redis. Containerization, version control, asynchronous communication and performance optimization."
   }
